876ed678ca0310116edcef2ab6863bb0fad7d0d3
MasudBinMazid <mamun15-5451@diu.edu.bd>
2025-03-16 21:06:00 +0600

    prblm8

--- lib/prblm8.dart
+++ lib/prblm8.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const NavigationDrawerApp());
-}
+void main() => runApp(const NavigationDrawerApp());
 
 class NavigationDrawerApp extends StatelessWidget {
   const NavigationDrawerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
@@ -23,46 +21,48 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Home Screen')),
-        backgroundColor: const Color.fromARGB(255, 204, 198, 228),
+        title: const Text('Home Screen', textAlign: TextAlign.center),
+        backgroundColor: const Color(0xFFCCC6E4),
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             const DrawerHeader(
-              decoration:
-                  BoxDecoration(color: Color.fromARGB(255, 134, 151, 6)),
+              decoration: BoxDecoration(color: Color(0xFF869706)),
               child: Text('Menu',
                   style: TextStyle(color: Colors.white, fontSize: 20)),
             ),
-            ListTile(
-              leading: const Icon(Icons.image),
-              title: const Text('Image Grid'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ImageGridScreen()),
-                );
-              },
+            _buildDrawerItem(
+              icon: Icons.image,
+              text: 'Image Grid',
+              onTap: () => _navigateTo(context, const ImageGridScreen()),
             ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SettingsScreen()),
-                );
-              },
+            _buildDrawerItem(
+              icon: Icons.settings,
+              text: 'Settings',
+              onTap: () => _navigateTo(context, const SettingsScreen()),
             ),
           ],
         ),
       ),
       body: const Center(child: Text('Welcome to the Home Screen!')),
     );
+  }
+
+  Widget _buildDrawerItem(
+      {required IconData icon,
+      required String text,
+      required VoidCallback onTap}) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(text),
+      onTap: onTap,
+    );
+  }
+
+  void _navigateTo(BuildContext context, Widget screen) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
   }
 }
 
@@ -73,40 +73,42 @@ class ImageGridScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Image Grid')),
+        title: const Text('Image Grid', textAlign: TextAlign.center),
         backgroundColor: Colors.blue,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // Number of columns
+            crossAxisCount: 2,
             crossAxisSpacing: 10.0,
             mainAxisSpacing: 10.0,
           ),
-          itemCount: 6, // Number of images
-          itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                  'https://media.istockphoto.com/id/1436977595/photo/contemporary-art-collage-conceptual-image-young-woman-feeling-sadness-concept-of-retro-style.jpg?s=2048x2048&w=is&k=20&c=_CijUOQcPep5AJ_R2LKlvf9I73bvYIhVJiQh2PxUNgI=',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            );
-          },
+          itemCount: 6,
+          itemBuilder: (context, index) => _buildImageItem(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImageItem() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.network(
+          'https://media.istockphoto.com/id/1436977595/photo/contemporary-art-collage-conceptual-image-young-woman-feeling-sadness-concept-of-retro-style.jpg?s=2048x2048&w=is&k=20&c=_CijUOQcPep5AJ_R2LKlvf9I73bvYIhVJiQh2PxUNgI=',
+          fit: BoxFit.cover,
         ),
       ),
     );
@@ -120,8 +122,8 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Settings')),
-        backgroundColor: const Color.fromARGB(255, 208, 67, 223),
+        title: const Text('Settings', textAlign: TextAlign.center),
+        backgroundColor: const Color(0xFFD043DF),
       ),
       body: const Center(child: Text('Settings Page')),
     );
